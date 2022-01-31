f58738a98989b579b64a7864dad3deca0a1b8ef2
AndresFlores <andresvaldo@gmail.com>
2022-01-31 20:42:05 -0300

    Ejercicio 2 completado

---
+++ Apoyo Desafío - JavaScript en práctica/assets/js/scriptEj2.js
@@ -0,0 +1,29 @@
+
+let canvas = document.getElementById("caja");
+
+let btnRojo = document.getElementById("btn-1");
+let btnNrnjo = document.getElementById("btn-2");
+let btnAmrlo = document.getElementById("btn-3");
+let btnVerde = document.getElementById("btn-4");
+let btnClste = document.getElementById("btn-5");
+let btnRosa = document.getElementById("btn-6");
+
+
+btnRojo.addEventListener("click", function () {
+    canvas.style.backgroundColor = "#e53e3e"    
+})
+btnNrnjo.addEventListener("click", function () {
+    canvas.style.backgroundColor = "#dd6b20"    
+})
+btnAmrlo.addEventListener("click", function () {
+    canvas.style.backgroundColor = "#faf089"    
+})
+btnVerde.addEventListener("click", function () {
+    canvas.style.backgroundColor = "#48bb78"    
+})
+btnClste.addEventListener("click", function () {
+    canvas.style.backgroundColor = "#81e6d9"    
+})
+btnRosa.addEventListener("click", function () {
+    canvas.style.backgroundColor = "#d53f8c"    
+})
